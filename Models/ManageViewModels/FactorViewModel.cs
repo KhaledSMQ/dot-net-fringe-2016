1bb6aa04ddf7cb99a0acf8f1fd078d80c23dea2b
Kassandra Perch <the@nodebotani.st>
2016-07-06 15:55:47 -0500

    Google Auth Added

---
+++ Models/ManageViewModels/FactorViewModel.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dot_net_core.Models.ManageViewModels
+{
+    public class FactorViewModel
+    {
+        public string Purpose { get; set; }
+    }
+}
